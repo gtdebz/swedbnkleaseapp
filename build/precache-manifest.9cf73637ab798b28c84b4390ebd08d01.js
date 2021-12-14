0430d080c0851dec1302abaecdaf2ac397b54ece
gtdebz <58554252+gtdebz@users.noreply.github.com>
2021-12-14 21:56:13 +0100

    Add files via upload

---
+++ build/precache-manifest.9cf73637ab798b28c84b4390ebd08d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf755cdc21acd5182e69eaed96e7bad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cb94b16ad7871c336c1",
+    "url": "/static/css/2.245dbd4c.chunk.css"
+  },
+  {
+    "revision": "ee1fbe8e516af402f14c",
+    "url": "/static/css/main.171dd008.chunk.css"
+  },
+  {
+    "revision": "4cb94b16ad7871c336c1",
+    "url": "/static/js/2.08189bec.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.08189bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1fbe8e516af402f14c",
+    "url": "/static/js/main.142b9aa6.chunk.js"
+  },
+  {
+    "revision": "adcaff0b7410d119f707",
+    "url": "/static/js/runtime-main.966486d9.js"
+  }
+]);
